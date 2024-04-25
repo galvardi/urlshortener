a49059bb658b935af3128fc5c2d079a13cf6413b
galvardi <gal.vardi2@mail.huji.ac.il>
2024-04-25 17:27:29 +0300

    hi

--- dyanmo_bd_manger.py
+++ dyanmo_bd_manger.py
@@ -2,9 +2,22 @@ import boto3
 
 
 class DynamoDBManager:
-    def __init__(self, table_name):
-        self.table_name = table_name
-        self.table = boto3.resource('dynamodb')
+    def __init__(self):
+        # self.table_name = table_name
+        self.db = boto3.resource('dynamodb')
 
-    def add_long_url(long_url: str, url_hash: str):
-        self.table.
+    def add_to_table(self, table_name: str, key: str, value: str):
+        table = self.db.Table(table_name)
+        table.put_item(Item={"long_url":key, "hashed_url":value})
+
+    def get_from_table(self,table_name: str, key):
+        table = self.db.Table(table_name)
+        response = table.get_item(Key={"long_url":key})
+        if not response.get("Item"): return None
+        return response["Item"]["hashed_url"]
+
+if __name__ == '__main__':
+    manager = DynamoDBManager()
+    print(manager.add_to_table("long-to-short","youtube.com", "akkss"))
+    print(manager.get_from_table("long-to-short", "youtube.com"))
+    print(manager.get_from_table("long-to-short", "youske.com"))
